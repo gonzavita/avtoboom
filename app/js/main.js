b08b28fbb1bd0fa4fe3e8f3bcf66a166765a6b17
gonzavita <gonzavita@yandex.ru>
2020-12-08 16:30:00 +0300

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,21 @@ $(function(){
 
 
 
+    var mixer =  mixitup('.products__inner-box');
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px"
+    });
+
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 2
+    });
+
+    
 
     
 });

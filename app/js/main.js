a0eff78ac5d3bd296bfb4702210d3c143e33e3a3
gonzavita <gonzavita@yandex.ru>
2020-12-17 11:11:04 +0300

    abayt contact, 404

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
 
 
@@ -16,16 +16,27 @@ $(function(){
         autoplaySpeed: 4000,
         speed: 2000
     });
-    $('.menu__btn').on('click', function(){
+    $('.slider__inner').slick();
+
+    $('.menu__btn').on('click', function () {
         $('.menu__list').slideToggle();
     });
-    
-    $('.header__btn-menu').on('click', function(){
+
+    $('.header__btn-menu').on('click', function () {
         $('.header__box').toggleClass('active');
     });
-  
+
+    $('.product-one__tabs .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
 
 
-    var mixer =  mixitup('.products__inner-box');
-    
+
+    var mixer = mixitup('.products__inner-box');
+
 });
